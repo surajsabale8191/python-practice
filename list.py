2841d950c7756d099221c9c2f5d54087d2d44015
surajsabale8191 <surajsabale8191@gmail.com>
2026-07-28 18:08:13 +0530

    sorted the list

--- list.py
+++ list.py
@@ -1,9 +1,13 @@
 numbers=[20,30,40,20,30,80,90,50,10,40,60]
 
-sort=[]
+sort1=[]
 
 for i in numbers:
-    if i not in sort:
-        sort.append(i)
+    if i not in sort1:
+        sort1.append(i)
 
-print(sort)
+print(sort1)
+
+sort1.sort()
+
+print(sort1)
