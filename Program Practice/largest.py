f57991601ba59bf35b22fc6a7820f3ad438a35ed
surajsabale8191 <surajsabale8191@gmail.com>
2026-08-03 17:56:55 +0530

    Basic Programs of python

---
+++ Program Practice/largest.py
@@ -0,0 +1,21 @@
+# list=[50,60,40]
+
+# list.sort()
+
+# print(list[2])
+
+num1=int(input("Enter the First number:"))
+num2=int(input("Enter the second number:"))
+num3=int(input("Enter the Third number:"))
+
+if num1>=num2 and num1>=num3:
+    largest=num1
+    
+elif num2>=num1 and num2>=num3:
+    largest=num2
+    
+else:
+    largest=num3
+    
+    
+print("Largest number is :",largest)
---
+++ Program Practice/swapping.py
@@ -0,0 +1,31 @@
+# # a = int(input("Enter first number: "))
+# # b = int(input("Enter second number: "))
+
+# # a = a + b
+# # b = a - b
+# # a = a - b
+
+# # print("After swapping:")
+# # print("a =", a)
+# # print("b =", b)
+
+
+# # 2)Ex 2 Using multiplication and Division 
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# a = a * b
+# b = a // b
+# a = a // b
+
+# print("a =", a)
+# print("b =", b)
+
+
+a = int(input("Enter first number a=: "))
+b = int(input("Enter second number b=: "))
+
+a, b = b, a
+
+print("a =", a)
+print("b =", b)
